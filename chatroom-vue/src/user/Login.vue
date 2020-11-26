b68a3326d7b58ea41403580dcaf55f4c12ae081c
lijunhon <812143550@qq.com>
2020-11-26 17:58:29 +0800

    first commit

--- chatroom-vue/src/user/Login.vue
+++ chatroom-vue/src/user/Login.vue
@@ -1,108 +1,162 @@
 <template>
-  <div class="login" clearfix>
-    <div class="login-wrap">
-      <el-row type="flex" justify="center">
-        <el-form ref="loginForm" :model="user" :rules="rules" status-icon label-width="80px">
-          <h3>登录</h3>
-          <hr>
-          <el-form-item prop="username" label="用户名">
-            <el-input v-model="user.username" placeholder="请输入用户名" prefix-icon></el-input>
-          </el-form-item>
-          <el-form-item id="password" prop="password" label="密码">
-            <el-input v-model="user.password" show-password placeholder="请输入密码"></el-input>
-          </el-form-item>
-          <router-link to="/">找回密码</router-link>
-          <router-link to="/register">注册账号</router-link>
-          <el-form-item>
-            <el-button type="primary" icon="el-icon-upload" @click="doLogin()">登 录</el-button>
-          </el-form-item>
-        </el-form>
-      </el-row>
+  <div class="login-wrap">
+    <div class="ms-title">登录管理系统</div>
+    <div class="ms-login">
+      <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="0px" class="demo-ruleForm">
+        <div v-if="errorInfo">
+          <span>{{errInfo}}</span>
+        </div>
+        <el-form-item prop="name">
+          <el-input v-model="ruleForm.name" placeholder="账号" ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input type="password" placeholder="密码" v-model="ruleForm.password" @keyup.enter.native="submitForm('ruleForm')"></el-input>
+        </el-form-item>
+        <el-form-item  prop="validate">
+          <el-input v-model="ruleForm.validate" class="validate-code" placeholder="验证码" ></el-input>
+          <div class="code" @click="refreshCode">
+            <s-identify :identifyCode="identifyCode"></s-identify>
+          </div>
+        </el-form-item>
+        <div class="login-btn">
+          <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+        </div>
+        <p style="font-size:14px;line-height:30px;color:#999;cursor: pointer;float:right;" @click="handleCommand()">注册</p>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 export default {
-  name: "index",
+  name: 'Login',
   data() {
     return {
-      user: {
-        username: "",
-        email: "",
-        password: ""
+      identifyCodes: "1234567890",
+      identifyCode: "",
+      errorInfo : false,
+      ruleForm: {
+        name: '',
+        password: '',
+        validate: ''
       },
-    };
-  },
-  created() {},
-  methods: {
-    doLogin() {
-      if (!this.user.username) {
-        this.$message.error("请输入用户名！");
-        return;
-      } else if (!this.user.password) {
-        this.$message.error("请输入密码！");
-        return;
-      } else {
-        //校验用户名和密码是否正确;
-        // this.$router.push({ path: "/personal" });
-        axios
-          .post("/login/", {
-            name: this.user.username,
-            password: this.user.password
-          })
-          .then(res => {
-            // console.log("输出response.data.status", res.data.status);
-            if (res.data.status === 200) {
-              this.$router.push({ path: "/personal" });
-            } else {
-              alert("您输入的用户名或密码错误！");
-            }
-          });
+      rules: {
+        name: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ],
+        validate: [
+          { required: true, message: '请输入验证码', trigger: 'blur' }
+        ]
       }
+    }
+  },
+  mounted() {
+    this.identifyCode = "";
+    this.makeCode(this.identifyCodes, 4);
+  },
+  methods: {
+    submitForm(formName) {
+      const self = this;
+      self.$refs[formName].validate((valid) => {
+        if (valid) {
+          localStorage.setItem('ms_username',self.ruleForm.name);
+          localStorage.setItem('ms_user',JSON.stringify(self.ruleForm));
+          console.log(JSON.stringify(self.ruleForm));
+          self.$http.post('/api/user/login',JSON.stringify(self.ruleForm))
+            .then((response) => {
+              console.log(response);
+              if (response.data == -1) {
+                self.errorInfo = true;
+                self.errInfo = '该用户不存在';
+                console.log('该用户不存在')
+              } else if (response.data == 0) {
+                console.log('密码错误')
+                self.errorInfo = true;
+                self.errInfo = '密码错误';
+              } else if (response.status == 200) {
+                self.$router.push('/readme');
+              }
+            }).then((error) => {
+            console.log(error);
+          })
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    handleCommand() {
+      this.$router.push('/register');
+    },
+    randomNum(min, max) {
+      return Math.floor(Math.random() * (max - min) + min);
+    },
+    refreshCode() {
+      this.identifyCode = "";
+      this.makeCode(this.identifyCodes, 4);
+    },
+    makeCode(o, l) {
+      for (let i = 0; i < l; i++) {
+        this.identifyCode += this.identifyCodes[
+          this.randomNum(0, this.identifyCodes.length)
+          ];
+      }
+      console.log(this.identifyCode);
     }
   }
 }
 </script>
 
 <style scoped>
-.login {
+.login-wrap{
+  position: absolute;
   background: url("../assets/images/login.jpg") no-repeat;
-  background-size: cover;
-  overflow: hidden;
+  width:100%;
+  height:100%;
 }
-.login-wrap {
-  background: url("../assets/images/login.jpg") no-repeat;
-  background-size: cover;
-  width: 400px;
-  height: 300px;
-  margin: 215px auto;
-  overflow: hidden;
-  padding-top: 10px;
-  line-height: 40px;
+.ms-title{
+  position: absolute;
+  top:50%;
+  width:100%;
+  margin-top: -230px;
+  text-align: center;
+  font-size:30px;
+  color: #fff;
+
 }
-#password {
-  margin-bottom: 5px;
+.ms-login{
+  position: absolute;
+  left:50%;
+  top:50%;
+  width:300px;
+  height:240px;
+  margin:-150px 0 0 -190px;
+  padding:40px;
+  border-radius: 5px;
+  background: #fff;
 }
-h3 {
-  color: #0babeab8;
-  font-size: 24px;
+.ms-login span {
+  color: red;
 }
-hr {
-  background-color: #444;
-  margin: 20px auto;
+.login-btn{
+  text-align: center;
 }
-a {
-  text-decoration: none;
-  color: #aaa;
-  font-size: 15px;
+.login-btn button{
+  width:100%;
+  height:36px;
 }
-a:hover {
-  color: coral;
+.code {
+  width: 112px;
+  height: 35px;
+  border: 1px solid #ccc;
+  float: right;
+  border-radius: 2px;
 }
-.el-button {
-  width: 80%;
-  margin-left: -50px;
+.validate-code {
+  width: 136px;
+  float: left;
 }
 </style>
